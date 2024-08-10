864e74fd8e4d227bc05453d03cd07cec5cf2734f
Zeeshan Ahmad <110332535+Zeeshier@users.noreply.github.com>
2024-08-10 11:19:01 +0500

    ab

---
+++ Abstraction/Abstract Employee.java
@@ -0,0 +1,33 @@
+abstract class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    abstract double calculateBonus();
+}
+
+class Manager extends Employee {
+    Manager(String name, double salary) {
+        super(name, salary);
+    }
+
+    @Override
+    double calculateBonus() {
+        return salary * 0.1;
+    }
+}
+
+class Developer extends Employee {
+    Developer(String name, double salary) {
+        super(name, salary);
+    }
+
+    @Override
+    double calculateBonus() {
+        return salary * 0.05;
+    }
+}
